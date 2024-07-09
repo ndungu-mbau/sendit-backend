2221a7fbaa9735d740727f2b50cd0cd090b01b97
kimwereafk <collinskimwere@gmail.com>
2024-07-09 16:12:18 +0300

    structure update

--- server/models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.orm import validates, relationship
-from sqlalchemy.ext.associationproxy import association_proxy
-
-db = SQLAlchemy()
-
-# Models go here!
