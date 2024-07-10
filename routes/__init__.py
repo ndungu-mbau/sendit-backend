58b9a4b4f614eb17ed7940303319f9efe397248a
kimwereafk <collinskimwere@gmail.com>
2024-07-10 15:23:37 +0300

    user update

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,24 @@
-from config import app, api
-from routes import user_routes, order_routes, item_routes, feedback_routes
+from flask import Blueprint
+from flask_restful import Api
+from .user_routes import UserResource
+from .order_routes import OrderResource
+from .item_routes import ItemResource
+from .feedback_routes import FeedbackResource
 
-api.add_resource(user_routes.UserResource, '/users', '/users/<int:id>')
-api.add_resource(order_routes.OrderResource, '/orders', '/orders/<int:id>')
-api.add_resource(item_routes.ItemResource, '/items', '/items/<int:id>')
-api.add_resource(feedback_routes.FeedbackResource, '/feedback', '/feedback/<int:id>')
+# Create a blueprint for routes
+bp = Blueprint('api', __name__)
+
+# Create an Api instance
+api = Api(bp)
+
+# Add resources to the API
+api.add_resource(UserResource, '/users', '/users/<int:id>')
+api.add_resource(OrderResource, '/orders', '/orders/<int:id>')
+api.add_resource(ItemResource, '/items', '/items/<int:id>')
+api.add_resource(FeedbackResource, '/feedback', '/feedback/<int:id>')
+
+# You can add more routes here as needed
+
+
+def init_app(app):
+    app.register_blueprint(bp)
