126e8ec4de68945223b5b1a67a67a681b35a125b
princess <princessmumbi001@gmail.com>
2024-07-10 09:57:48 +0300

    commit

---
+++ routes/user_routes.py
@@ -0,0 +1,23 @@
+from flask_restful import Resource
+from config import db
+from models import User
+
+class UserResource(Resource):
+    def get(self, id=None):
+        if id:
+            user = User.query.get(id)
+            return user.to_dict()
+        users = User.query.all()
+        return [user.to_dict() for user in users]
+
+    def post(self):
+        # 
+        pass
+
+    def put(self, id):
+        # Handle user update
+        pass
+
+    def delete(self, id):
+        # Handle user deletion
+        pass
