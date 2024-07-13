eba54450bbb2e4088e86dbdda6886154c8e2f482
princess <princessmumbi001@gmail.com>
2024-07-14 01:31:20 +0300

    seeding data

--- seed.py
+++ seed.py
@@ -8,28 +8,28 @@ fake = Faker()
 def seed_data():
     with app.app_context():
         try:
-            
+
             User.query.delete()
             Order.query.delete()
             Item.query.delete()
             Feedback.query.delete()
 
-          
+
             users = []
-            for _ in range(10):  
+            for _ in range(10):
                 username = fake.user_name()
                 domain = fake.free_email_domain()
                 email = f"{username}@{domain}"
-                password = fake.password(length=12)  
+                password = fake.password(length=12)
                 role = fake.random_element(elements=['admin', 'user'])
                 users.append(User(username=username, email=email, password=password, role=role))
-            
+
             db.session.add_all(users)
             db.session.commit()
 
-            
+
             items = []
-            for _ in range(10): 
+            for _ in range(10):
                 items.append(Item(
                     item_name=fake.word(),
                     description=fake.text(),
@@ -39,10 +39,10 @@ def seed_data():
             db.session.add_all(items)
             db.session.commit()
 
-            
+
             orders = []
             for user in users:
-                for _ in range(2): 
+                for _ in range(2):
                     order = Order(
                         pickup_address=fake.address(),
                         delivery_address=fake.address(),
@@ -50,7 +50,7 @@ def seed_data():
                     )
                     orders.append(order)
                     db.session.add(order)
-                    
+
                     # Adding items
                     order_items = fake.random_elements(elements=[item.id for item in items], length=2, unique=True)
                     for item_id in order_items:
